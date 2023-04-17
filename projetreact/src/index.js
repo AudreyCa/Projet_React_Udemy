7b60cc498e384cf24ab61903290b1483a4095bbe
audca <a.cannesson5@gmail.com>
2023-04-17 17:59:35 +0200

    début du projet (avant projets)

--- projetreact/src/index.js
+++ projetreact/src/index.js
@@ -1,14 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Header from './Header';
+import User from './User';
 
-// selection de l'élément du parent div "root" dans index.html
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <h1 style={{ color: red }}>Hello world</h1>
-  </React.StrictMode>
-  
+  // <React.StrictMode></React.StrictMode>
+  <>
+  <User name="John" expertise="Dev" />
+  <User name="Marc" expertise="DevOps" />
+  <User name="Bob" expertise="Engineer" />
+  </>
+ 
 );
+
+
+
+
+
+// // Variable pour le style
+// const main = {
+//   backgroundColor:'green',
+//   height:'100vh',
+//   width: '100%' 
+// }
+
+// const firstvariable = "Expert";
+// const secondvariable = "Developper";
+
+// // selection de l'élément du parent div "root" dans index.html
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   // <React.StrictMode></React.StrictMode>
+//   <>
+//     <div style={ main }>
+//       <h1>Hello world</h1>
+
+//       <h2>
+//         We are {firstvariable + ' ' + secondvariable}
+//         {/* {`We are ${firstvariable}  ${secondvariable}`} */}
+//       </h2>
+//     </div>
+//   </>
+ 
+// );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

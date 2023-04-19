f02186768758ef140090163f7237737480f18f9c
audca <a.cannesson5@gmail.com>
2023-04-19 17:52:51 +0200

    array.map et material UI

--- projetreact/src/App2.js
+++ projetreact/src/App2.js
@@ -1,12 +1,37 @@
 import React from "react";
 
-import Pokemon from "./Components/Pokemon";
+// import Pokemon from "./Components/Pokemon";
+import AcUnitOutlinedIcon from '@mui/icons-material/AcUnitOutlined';
 
+const Array = [
+    {
+        name: "John",
+        expertise: "Frontend",
+        Experience: "3 ans"
+    },
+    {
+        name: "Marc",
+        expertise: "Backend",
+        Experience: "4 ans"
+    },
+    {
+        name: "Sarah",
+        expertise: "DevOps",
+        Experience: "6 ans"
+    }
+]
 
 function App2() {
     return (
         <>
-        <Pokemon />
+        {/* <Pokemon /> */}
+        <p>Personal information :</p>
+        <p>{Array.map(
+            (element) => { 
+                return (<p>{element.name}</p>)
+            }
+            )}</p>
+<AcUnitOutlinedIcon />
         </>
     );
 }
